24e2d976403ba7a450e0bba0b1371f9e164bb31c
Quang Nguyen <Quang Nguyen@DESKTOP-SJLP19O>
2017-02-20 22:33:23 +0700

    ok

--- PolymorphismDemo/src/com/demo/Fish.java
+++ PolymorphismDemo/src/com/demo/Fish.java
@@ -5,6 +5,6 @@ public class Fish extends Animal {
 	//Ghi đè phương thức khi extends class
 	@Override
 	public void introduce() {
-		System.out.println("I'm Fish");
+		System.out.println("I'm Fish 123");
 	}
 }
