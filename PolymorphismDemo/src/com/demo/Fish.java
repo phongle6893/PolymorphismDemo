967e9940af9d75186ad5ba5c07c18c0b88618dad
Lê Hồng Phong <phongle6893@gmail.com>
2017-02-21 13:00:57 +0700

    Update Fish.java
    update

--- PolymorphismDemo/src/com/demo/Fish.java
+++ PolymorphismDemo/src/com/demo/Fish.java
@@ -5,7 +5,6 @@ public class Fish extends Animal {
 	//Ghi đè phương thức khi extends class
 	@Override
 	public void introduce() {
-		System.out.println("hello, I'm Fish 123");
-		System.out.println("hello, I'm Fish 123");
+		System.out.println("I'm Fish");
 	}
 }
